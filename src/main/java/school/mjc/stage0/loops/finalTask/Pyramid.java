4599c19eb970f2ac6f88d47b0c81266dd372c8f2
Sasha-PC\Sasha <aleksandr.v.g.m@gmail.com>
2022-08-04 13:09:59 +0300

    solution Module #4.2.Loops. Final Task

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,6 +2,23 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        int left = cathetusLength;
+        int right = cathetusLength;
+        for (int i = 1; i <= cathetusLength; i ++){
+            int digit = i;
+            for (int j = 1; j < 2 * cathetusLength; j++){
+                if(j >= left && j <= right){
+                    System.out.print(digit);
+                    digit = j < cathetusLength ? --digit : ++digit;
+                } else if (j > right) {
+                    break;
+                } else
+                    System.out.print(' ');
+            }
+            left--;
+            right++;
+            System.out.println();
+        }
 
     }
 
